3109033a76afcbfa559b317301baa058e4d43373
GammaSpark <jtguerrero25@gmail.com>
2023-01-26 06:55:31 -0600

    Completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,10 +32,11 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtPrice = new System.Windows.Forms.TextBox();
-            this.txtTaxRate = new System.Windows.Forms.TextBox();
             this.txtTaxDue = new System.Windows.Forms.TextBox();
             this.btnCalculate = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
+            this.scaleTaxRate = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.scaleTaxRate)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -71,56 +72,77 @@
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.Size = new System.Drawing.Size(100, 23);
             this.txtPrice.TabIndex = 3;
+            this.txtPrice.Text = "$0.00";
             this.txtPrice.TextChanged += new System.EventHandler(this.txtPrice_TextChanged);
             this.txtPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrice_KeyPress);
-            // 
-            // txtTaxRate
-            // 
-            this.txtTaxRate.Location = new System.Drawing.Point(165, 150);
-            this.txtTaxRate.Name = "txtTaxRate";
-            this.txtTaxRate.Size = new System.Drawing.Size(100, 23);
-            this.txtTaxRate.TabIndex = 4;
+            this.txtPrice.Leave += new System.EventHandler(this.txtPrice_Leave);
             // 
             // txtTaxDue
             // 
             this.txtTaxDue.Location = new System.Drawing.Point(165, 200);
             this.txtTaxDue.Name = "txtTaxDue";
+            this.txtTaxDue.ReadOnly = true;
             this.txtTaxDue.Size = new System.Drawing.Size(100, 23);
             this.txtTaxDue.TabIndex = 5;
+            this.txtTaxDue.Text = "$0.00";
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(100, 321);
+            this.btnCalculate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalculate.Location = new System.Drawing.Point(100, 291);
             this.btnCalculate.Name = "btnCalculate";
-            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.Size = new System.Drawing.Size(104, 53);
             this.btnCalculate.TabIndex = 6;
             this.btnCalculate.Text = "Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(253, 321);
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClear.Location = new System.Drawing.Point(263, 291);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.Size = new System.Drawing.Size(104, 53);
             this.btnClear.TabIndex = 7;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // scaleTaxRate
+            // 
+            this.scaleTaxRate.DecimalPlaces = 2;
+            this.scaleTaxRate.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.scaleTaxRate.Location = new System.Drawing.Point(165, 153);
+            this.scaleTaxRate.Name = "scaleTaxRate";
+            this.scaleTaxRate.ReadOnly = true;
+            this.scaleTaxRate.Size = new System.Drawing.Size(100, 23);
+            this.scaleTaxRate.TabIndex = 8;
+            this.scaleTaxRate.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(499, 446);
+            this.Controls.Add(this.scaleTaxRate);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnCalculate);
             this.Controls.Add(this.txtTaxDue);
-            this.Controls.Add(this.txtTaxRate);
             this.Controls.Add(this.txtPrice);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.scaleTaxRate)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,9 +154,9 @@
         private Label label2;
         private Label label3;
         private TextBox txtPrice;
-        private TextBox txtTaxRate;
         private TextBox txtTaxDue;
         private Button btnCalculate;
         private Button btnClear;
+        private NumericUpDown scaleTaxRate;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,11 +2,13 @@ namespace Tax_calculator
 {
     public partial class Form1 : Form
     {
+        decimal taxDue = 0;
+        
         public Form1()
         {
             InitializeComponent();
         }
-
+        
         private void txtPrice_TextChanged(object sender, EventArgs e)
         {
 
@@ -24,6 +26,51 @@ namespace Tax_calculator
             {
                 e.Handled = true;
             }
+        }
+
+        private void txtPrice_Leave(object sender, EventArgs e)
+        {
+            if (txtPrice.Text != String.Empty)
+            {
+                if (!txtPrice.Text.Contains("$"))
+                {
+                    txtPrice.Text = "$" + Math.Round(Convert.ToDouble(txtPrice.Text), 2);
+                }
+                else
+                {
+                    txtPrice.Text = "$" + Math.Round(Convert.ToDouble(txtPrice.Text.Substring(1)), 2);
+                }
+            }
+        }
+        private void reset_all()
+        {
+            txtPrice.Text = "$0.00";
+            txtTaxDue.Text = "$0.00";
+            txtPrice.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            reset_all();
+        }
+        private void getTaxDue()
+        {
+            if (txtPrice.Text.Contains("$"))
+                taxDue = Convert.ToDecimal(txtPrice.Text.Substring(1));
+
+
+            
+            
+                taxDue = Convert.ToDecimal(txtPrice.Text.Substring(1)) * (scaleTaxRate.Value / 100);
+
+            
+
+            txtTaxDue.Text = taxDue.ToString();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            getTaxDue();
         }
     }
 }
